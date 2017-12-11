edb0b0c31354469da21abe0c9c200fcedd32e508
Steve Hobbs <elkdanger@gmail.com>
2017-12-11 22:59:52 +0000

    Added header and styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    
+    <div class="page-header">
+      <h1>Kanban Board</h1>
+      <p class="lead">An example of a Kanban board, written in VueJS</p>
+    </div>
+
     <router-view/>
   </div>
 </template>
@@ -7,5 +13,11 @@
 <script>
 export default {
   name: 'app'
-}
+};
 </script>
+
+<style>
+.page-header h1 {
+  font-weight: 300;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -10,4 +10,4 @@ export default new Router({
       name: 'Default'
     }
   ]
-})
+});
