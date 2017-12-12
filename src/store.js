d09f2832bde7e3b8f64fe2e96dc8830c9fc0d566
Steve Hobbs <elkdanger@gmail.com>
2017-12-12 22:52:31 +0000

    Added store mutation for addItem

--- src/store.js
+++ src/store.js
@@ -3,11 +3,18 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+/* eslint-disable no-param-reassign */
 export default new Vuex.Store({
   state: {
     items: {
       todo: []
     },
     nextId: 1
+  },
+  mutations: {
+    addItem(state, item) {
+      state.items.todo.push(Object.assign(item, { id: state.nextId }));
+      state.nextId += 1;
+    }
   }
 });
