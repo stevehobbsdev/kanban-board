ecb94c61532f47b0c9cfc34d62617a34d787aed6
Steve Hobbs <elkdanger@gmail.com>
2018-01-17 23:18:34 +0000

    Implemented vuedraggable

--- src/components/TaskLane.vue
+++ src/components/TaskLane.vue
@@ -2,9 +2,11 @@
   <div class="card">
       <h3 class="card-header">{{ title }}</h3>
       <div class="card-body">
+        <draggable v-model="draggables" :options="{ group: 'default' }">
           <div v-for="item in items" :key="item.id">
               <item :item="item"></item>
           </div>
+        </draggable>
       </div>
       <div class="card-footer text-muted">
           {{itemCount}}
@@ -13,20 +15,39 @@
 </template>
 
 <script>
+import Draggable from 'vuedraggable';
 import TaskLaneItem from './TaskLaneItem';
 
 export default {
   name: 'TaskLane',
-  props: ['items', 'title'],
+  props: ['items', 'title', 'id'],
   components: {
-    item: TaskLaneItem
+    item: TaskLaneItem,
+    draggable: Draggable
   },
   computed: {
     itemCount() {
       if (!this.items) return '';
       if (this.items.length === 1) return '1 task';
       return `${this.items.length} tasks`;
+    },
+    draggables: {
+      get() {
+        return this.items;
+      },
+      set(items) {
+        this.$store.commit('updateItems', {
+          items,
+          id: this.id
+        });
+      }
     }
   }
 };
 </script>
+
+<style>
+.card-body > * {
+  min-height: 50px;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -17,6 +17,9 @@ export default new Vuex.Store({
     addItem(state, item) {
       state.items.todo.push(Object.assign(item, { id: state.nextId }));
       state.nextId += 1;
+    },
+    updateItems(state, { items, id }) {
+      state.items[id] = items;
     }
   }
 });
