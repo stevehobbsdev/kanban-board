12f2dbad4ae741f336d14bc0584e5f439124d0c9
Steve Hobbs <elkdanger@gmail.com>
2018-01-24 22:20:00 +0000

    Added lane status badges to backlog view

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -4,8 +4,8 @@
 
     <div class="card" v-for="item in items" :key="item.id">
         <div class="card-block">
-            <h5 class="card-title"><span class="text-muted">#{{item.id}}</span>
-            {{item.text}}</h5>
+          <h5 class="card-title"><span class="text-muted">#{{item.id}}</span>
+          {{item.text}} <span :class="badgeClass(item)">{{badgeText(item)}}</span></h5>
         </div>
     </div>
   </div>
@@ -15,14 +15,52 @@
 import { mapState } from 'vuex';
 import NewItemForm from './NewItemForm';
 
+const badgeDetail = {
+  todo: {
+    text: 'todo',
+    class: 'badge badge-light'
+  },
+
+  inProgress: {
+    text: 'in progress',
+    class: 'badge badge-info'
+  },
+
+  done: {
+    text: 'done',
+    class: 'badge badge-success'
+  }
+};
+
 export default {
   name: 'Backlog',
   components: {
     'new-item': NewItemForm
   },
   computed: mapState({
-    items: s => s.items.todo
-  })
+    items: s => [...s.items.todo, ...s.items.inProgress, ...s.items.done]
+  }),
+  methods: {
+    itemLane(item) {
+      if (this.$store.state.items.todo.includes(item)) {
+        return 'todo';
+      } else if (this.$store.state.items.inProgress.includes(item)) {
+        return 'inProgress';
+      }
+
+      return 'done';
+    },
+
+    badgeText(item) {
+      const lane = this.itemLane(item);
+      return badgeDetail[lane].text;
+    },
+
+    badgeClass(item) {
+      const lane = this.itemLane(item);
+      return badgeDetail[lane].class;
+    }
+  }
 };
 </script>
 
